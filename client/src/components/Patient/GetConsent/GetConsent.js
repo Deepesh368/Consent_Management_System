8cc4a55efc8ed0d1dbd6b9f25ac55f923909522b
Mohd. Rizwan Shaikh <mohdrizwan.shaikh2112@gmail.com>
2023-03-03 10:30:29 +0530

    Frontend for create consent request implemented

--- client/src/components/Patient/GetConsent/GetConsent.js
+++ client/src/components/Patient/GetConsent/GetConsent.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
-import viewconsents from '../../../actions/patient';
+import { viewconsents } from '../../../actions/patient';
 
 const GetConsent = () => {
     const dispatch = useDispatch();
