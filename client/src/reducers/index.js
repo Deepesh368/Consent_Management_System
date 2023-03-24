a9f3f44f9e117344a742cd86ff96f091625b8276
Mohd. Rizwan Shaikh <mohdrizwan.shaikh2112@gmail.com>
2023-03-24 17:43:25 +0530

    Three frontend arc implemented

--- client/src/reducers/index.js
+++
@@ -1,5 +0,0 @@
-import { combineReducers } from 'redux';
-import patientAuth from './patientAuth';
-import consent from './consent';
-
-export default combineReducers({ patientAuth, consent });
---
+++ patient/src/components/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>
+                This is Home page of the application. Main Entry Point. Also the
+                frontend for patient.
+            </h1>
+        </div>
+    );
+};
+
+export default Home;
