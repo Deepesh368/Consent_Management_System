f94b284727ab555b7c35aa224f00b69a2e5a9668
maruthisriram <maruthisriram3@gmail.com>
2023-03-28 18:56:17 +0530

    Removed HEAD and commit if lines from code

--- patient/src/App.js
+++ patient/src/App.js
@@ -4,14 +4,9 @@ import { Container } from '@material-ui/core';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 import Home from './components/Home/Home';
-<<<<<<< HEAD
 import Login from './components/Login/login.js'
 import PatientDashBoard from './components/PatientDashboard/patientDashboard.js';
 import Profile from './components/PatientProfile/profile.js'
-=======
-import Patientregister from './components/Register/Patientregister';
-
->>>>>>> ad0b21f03826f00efdee0bf581abe091c803dc38
 const App = () => {
     const user = JSON.parse(localStorage.getItem('profile'));
 
@@ -19,7 +14,6 @@ const App = () => {
         <BrowserRouter>
             <Container maxWidth="xl">
                 <Routes>
-<<<<<<< HEAD
                     <Route path="/maruthi" exact element={<PatientDashBoard />} />
                 </Routes>
                 <Routes>
@@ -27,10 +21,6 @@ const App = () => {
                 </Routes>
                 <Routes>
                     <Route path="/" exact element={<Home />} />
-=======
-                    <Route path="/" element={<Home />} />
-                    <Route path="/patient_register" element={<Patientregister />} />
->>>>>>> ad0b21f03826f00efdee0bf581abe091c803dc38
                 </Routes>
                 <Routes>
                     <Route path="/Login" exact element={<Login />} />
