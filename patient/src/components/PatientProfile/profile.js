ca51c853ef8e116a2f877c12c947a903ff97e4d5
maruthisriram <maruthisriram3@gmail.com>
2023-03-28 21:20:39 +0530

    Profile Pagee added Edit Button

--- patient/src/components/PatientProfile/profile.js
+++ patient/src/components/PatientProfile/profile.js
@@ -1,7 +1,9 @@
-import { Toolbar, TextField, } from "@material-ui/core";
+import { Toolbar, TextField, Typography, Button, } from "@material-ui/core";
 import React from "react";
 import Header from './navbar.js'
 import './styles.css'
+import ModeEditIcon from '@mui/icons-material/ModeEdit';
+
 const Profile = () =>{
     return (
         <div>
@@ -9,12 +11,20 @@ const Profile = () =>{
             <Toolbar />
             <Toolbar />
             <Toolbar />
-
             <TextField InputProps={{ disableUnderline: true }} style={{background:'#20CD51', borderRadius: 50, 
-            margin:'auto auto 1% 40%', height:'40px', width:"20%", color:"white"} } placeholder='   Name' fullWidth required />
+            margin:'auto auto 1% 40%', height:'40px', width:"20%",
+            textAlign:"center"
+            }} 
+            placeholder='   Name' fullWidth required />
+            <Button ><ModeEditIcon /></Button>
             <TextField InputProps={{ disableUnderline: true }} style={{background:'#20CD51', borderRadius: 50, margin:'1% auto 1% 40%', height:'40px',  width:"20%"}} placeholder='   Email' type='Email' fullWidth required />
+            <Button ><ModeEditIcon /></Button>
+
             <TextField InputProps={{ disableUnderline: true }} style={{background:'#20CD51', borderRadius: 50, margin:'1% auto 1% 40%', height:'40px',  width:"20%"}} placeholder='   Password' type='Password' fullWidth required />
+            <Button ><ModeEditIcon /></Button>
+
             <TextField InputProps={{ disableUnderline: true }} style={{background:'#20CD51', borderRadius: 50, margin:'1% auto auto 40%', height:'40px',  width:"20%"}} placeholder='   Phone Number' type='number' fullWidth required />
+            <Button ><ModeEditIcon /></Button>
         </div>
     )
 }
