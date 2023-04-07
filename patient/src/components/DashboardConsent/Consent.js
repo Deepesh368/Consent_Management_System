6c4c8da804378505393a2d7ac4fd17334dea5c7e
Mohd. Rizwan Shaikh <mohdrizwan.shaikh2112@gmail.com>
2023-04-07 23:53:32 +0530

    Integration of consent dashboard done

--- patient/src/components/DashboardConsent/Consent.js
+++ patient/src/components/DashboardConsent/Consent.js
@@ -1,34 +1,34 @@
-import React from "react";
+import React from 'react';
 import Card from '@mui/material/CardContent';
-import { CardContent, CardHeader, Typography, Button } from "@material-ui/core";
+import { CardContent, CardHeader, Typography, Button } from '@material-ui/core';
 import Paper from '@mui/material/Paper';
 
-const Consent = (props) =>{
-
+const Consent = (props) => {
     return (
         <div>
-        <Button style={{ maxWidth: '100%', textAlign:"left"}}>
-            <Paper variant="outlined" elevation={3} sx={{background:"LightYellow"}}>
-                <Card variant="outlined">
-                <CardHeader 
-                    title={"Consent Information - " + props.consentNum} 
-                    subheader="September 14, 2016"
-
-                />
-                <CardContent>
-                    <Typography variant="body2" color="text.secondary">
-                        <h3>Patient ID:</h3>
-                        <h3>Doctor ID:</h3>
-                        <h3>Hospital ID:</h3>
-
-                    </Typography>
-                </CardContent>
-                </Card>
-            </Paper>
-        </Button>
-           
+            <Button style={{ maxWidth: '100%', textAlign: 'left' }}>
+                <Paper
+                    variant="outlined"
+                    elevation={3}
+                    sx={{ background: 'LightYellow' }}
+                >
+                    <Card variant="outlined">
+                        <CardHeader
+                            title={'Consent Information - ' + props.consentNum}
+                            subheader="September 14, 2016"
+                        />
+                        <CardContent>
+                            <Typography variant="body2" color="text.secondary">
+                                <h3>Patient ID:</h3>
+                                <h3>Doctor ID:</h3>
+                                <h3>Hospital ID:</h3>
+                            </Typography>
+                        </CardContent>
+                    </Card>
+                </Paper>
+            </Button>
         </div>
-    )
-}
+    );
+};
 
 export default Consent;
--- patient/src/components/DashboardConsent/consentDashBoard.js
+++ patient/src/components/DashboardConsent/consentDashBoard.js
@@ -1,24 +1,54 @@
-import { Toolbar, Grid } from "@material-ui/core";
-import React from "react";
-import Header from './Navbar.js'
-import Consent from "./Consent.js";
+import { Toolbar, Grid, CircularProgress } from '@material-ui/core';
+import React from 'react';
+import Header from './Navbar.js';
+import Consent from './Consent.js';
+import { useSelector } from 'react-redux';
+import decode from 'jwt-decode';
 
-const ConsentDashBoard = () =>{
-    let cards = []
-    for(let i=0;i<20;i++){
+import { getAllConsents } from '../../actions/consent.js';
+
+const ConsentDashBoard = () => {
+    const { consents, isLoading } = useSelector((state) => state.consents);
+    const [user, setUser] = useState(
+        JSON.parse(localStorage.getItem('patient'))
+    );
+
+    useEffect(() => {
+        const token = user?.token;
+        if (token) {
+            const decodedToken = decode(token);
+            const patient_id = decodedToken.id;
+            dispatch(getAllConsents(patient_id));
+        }
+    }, []);
+
+    let cards = [];
+    for (let i = 0; i < 20; i++) {
         cards.push(i);
     }
-    let status = "green"
-    return (
+    let status = 'green';
+
+    if (!consents.length && !isLoading) return 'No consent requests';
+
+    return isLoading ? (
+        <CircularProgress />
+    ) : (
         <div>
             <Header />
             <Toolbar />
-            <Grid style={{marginTop:"2%"}} container direction="row" justifyContent="center" alignItems="center">
-                {cards.map((id)=><Consent key={id}   consentNum={id} status={status} />)}
+            <Grid
+                style={{ marginTop: '2%' }}
+                container
+                direction="row"
+                justifyContent="center"
+                alignItems="center"
+            >
+                {cards.map((id) => (
+                    <Consent key={id} consentNum={id} status={status} /> //NEED TO PASS CONSENT PROPERTIES
+                ))}
             </Grid>
-
         </div>
-    )
-}
+    );
+};
 
-export default ConsentDashBoard
+export default ConsentDashBoard;
