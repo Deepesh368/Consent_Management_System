f2a98ae9437d356037ea5a1517f25c0fbe4ed9e6
maruthisriram <maruthisriram3@gmail.com>
2023-04-07 11:49:15 +0530

    doctor login dashboard and home page

--- doctor/src/components/Login/Header.js
+++ doctor/src/components/Login/Header.js
@@ -1,23 +1,53 @@
-import React from "react";
-import {AppBar, Toolbar, Typography, Button} from "@mui/material";
+import React from 'react';
+import { AppBar, Toolbar, Typography, Button } from '@mui/material';
 import MedicalServicesIcon from '@mui/icons-material/MedicalServices';
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import { useNavigate } from 'react-router-dom';
+import { Box } from '@material-ui/core';
 
 const Header = () => {
-    return(
-        <React.Fragment>
-            <AppBar sx={{background: "#10BB40", minHeight: '100px'}}>
-                <Toolbar>
-                    <Button disableElevation='true' sx={{"&:hover": {backgroundColor: "#10EB40"}, margin:'5px', background:"#10BB40"}} variant="contained"><ArrowBackIcon /> </Button>
-                    < MedicalServicesIcon />
-                    <Typography marginLeft={2}  sx={{fontWeight: 'bold', fontSize: 28}}>
-                        Swastha Suraksha
-                    </Typography>
+    let navigate = useNavigate();
 
-                    <h2 style={{width:'10%', margin:'0px 0px 0px 970px'}}>Patient Login </h2>
+    return (
+        <>
+            <AppBar sx={{ background: '#10BB40', minHeight: '70px' }}>
+                <Toolbar>
+                    <Button
+                        disableElevation="true"
+                        sx={{
+                            '&:hover': { backgroundColor: '#10EB40' },
+                            margin: '2px',
+                            background: '#10BB40',
+                        }}
+                        variant="contained"
+                        onClick={() => navigate(-1)}
+                    >
+                        <ArrowBackIcon />{' '}
+                    </Button>
+                    <Button
+                        // color="primary"
+                        sx={{ color: 'white' }}
+                        size="large"
+                        startIcon={<MedicalServicesIcon />}
+                        onClick={() => navigate('/')}
+                    >
+                        Swastha Suraksha
+                    </Button>
+                    <Box
+                        m={1}
+                        //margin
+                        display="flex"
+                        justifyContent="flex-end"
+                        alignItems="flex-end"
+                        sx={{ flexGrow: 1 }}
+                    >
+                        <h2 style={{ width: '20%', marginLeft: 'auto' }}>
+                            Doctor Login
+                        </h2>
+                    </Box>
                 </Toolbar>
             </AppBar>
-        </React.Fragment>
+        </>
     );
 };
 
