2365dc6daf9744743efad3215e2ca24762ca23b5
Deepesh368 <kvvdeepesh@gmail.com>
2023-05-06 00:11:26 +0530

    Made changes to consent request fields

--- patient/src/components/DashboardConsent/Consent.js
+++ patient/src/components/DashboardConsent/Consent.js
@@ -43,22 +43,78 @@ const Consent = (props) => {
                     <CardHeader title={'Consent Information'} />
                     <CardContent>
                         <Typography variant="body2" color="text.secondary">
-                            <h3>requestId: {props.requestId}</h3>
-                            <h3>doctorId={props.doctorId}</h3>
-                            <h3>patientId={props.patientId}</h3>
+                            {/* <h3>requestId: {props.requestId}</h3> */}
+                            <h3>doctorId: {props.doctorId}</h3>
+                            {/* <h3>patientId: {props.patientId}</h3> */}
                             <h3>
-                                requestingHospitalId=
+                                requestingHospitalId:
                                 {props.requestingHospitalId}
                             </h3>
-                            <h3>sendingHospitalId={props.sendingHospitalId}</h3>
-                            <h3>status={props.status}</h3>
-                            <h3>dateofRequest={props.dateofRequest}</h3>
-                            <h3>reqStartDate={props.reqStartDate}</h3>
-                            <h3>reqEndDate={props.reqEndDate}</h3>
-                            <h3>reqValidity={props.reqValidity}</h3>
-                            <h3>consentStartDate={props.consentStartDate}</h3>
-                            <h3>consentEndDate={props.consentEndDate}</h3>
-                            <h3>consentValidity={props.consentValidity}</h3>
+                            <h3>
+                                sendingHospitalId: {props.sendingHospitalId}
+                            </h3>
+                            <h3>status: {props.status}</h3>
+                            <h3>dateofRequest: {props.dateofRequest}</h3>
+                            <h3>
+                                reqStartDate:{' '}
+                                {props.reqStartDate
+                                    ? props.reqStartDate
+                                          .slice(0, 10)
+                                          .split('-')
+                                          .reverse()
+                                          .join('-')
+                                    : props.reqStartDate}
+                            </h3>
+                            <h3>
+                                reqEndDate:{' '}
+                                {props.reqEndDate
+                                    ? props.reqEndDate
+                                          .slice(0, 10)
+                                          .split('-')
+                                          .reverse()
+                                          .join('-')
+                                    : props.reqEndDate}
+                            </h3>
+                            <h3>
+                                reqValidity:{' '}
+                                {props.reqValidity
+                                    ? props.reqValidity
+                                          .slice(0, 10)
+                                          .split('-')
+                                          .reverse()
+                                          .join('-')
+                                    : props.reqValidity}
+                            </h3>
+                            <h3>
+                                consentStartDate:{' '}
+                                {props.consentStartDate
+                                    ? props.consentStartDate
+                                          .slice(0, 10)
+                                          .split('-')
+                                          .reverse()
+                                          .join('-')
+                                    : props.consentStartDate}
+                            </h3>
+                            <h3>
+                                consentEndDate:{' '}
+                                {props.consentEndDate
+                                    ? props.consentEndDate
+                                          .slice(0, 10)
+                                          .split('-')
+                                          .reverse()
+                                          .join('-')
+                                    : props.consentEndDate}
+                            </h3>
+                            <h3>
+                                consentValidity:{' '}
+                                {props.consentValidity
+                                    ? props.consentValidity
+                                          .slice(0, 10)
+                                          .split('-')
+                                          .reverse()
+                                          .join('-')
+                                    : props.consentValidity}
+                            </h3>
                         </Typography>
                     </CardContent>
                 </Card>
