57bce5c85f770cd43d9dbbdc0f36efa630729253
Deepesh368 <kvvdeepesh@gmail.com>
2023-05-06 17:40:29 +0530

    Create consent fixed and also navbar is fixed

--- patient/src/components/EditConsent/navbar.js
+++ patient/src/components/EditConsent/navbar.js
@@ -20,7 +20,7 @@ const Header = () => {
 
     return(
         <React.Fragment>
-            <AppBar sx={{background: "#10BB40", minHeight: '15%'}}>
+            <AppBar sx={{background: "#10BB40", minHeight: '7%'}}>
                 <Toolbar>
                     <Button disableElevation='true' sx={{"&:hover": {backgroundColor: "#10EB40"}, margin:'0px 2px 0 -25px', background:"#10BB40"}} variant="contained" onClick={() => navigate(-1)}><ArrowBackIcon /> </Button>
                     <Button
@@ -35,7 +35,7 @@ const Header = () => {
                     <Button onClick={()=>navigate("/patient/records")} sx={{"&:hover": {backgroundColor: "#D3D3D3"}, width:"12%", marginLeft:'58%', borderRadius:'20px', background:"white", color:"black"}} variant="contained">Hospital Records</Button>
                     <Button onClick={()=>navigate("/patient")} sx={{"&:hover": {backgroundColor: "#D3D3D3"}, marginLeft:'auto', borderRadius:'20px', background:"white", color:"black"}} variant="contained">DashBoard</Button>
                     <Button sx={{"&:hover": {backgroundColor: "#8A0717"}, marginLeft:'10px', borderRadius:'20px', background:"red", color:"white"}} onClick={logout} variant="contained">Log out</Button>
-                    </Toolbar>
+                </Toolbar>
                 <Typography marginLeft={4}  sx={{fontWeight: 'bold', fontSize: 16}}>
                     ABHA ID: {ABHA_NUM}
                 </Typography>
