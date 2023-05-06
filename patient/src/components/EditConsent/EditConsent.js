91041efe7a7865930563762c3805458e312060bc
Deepesh368 <kvvdeepesh@gmail.com>
2023-05-07 00:40:00 +0530

    Multiple Instances of hospital implemented

--- patient/src/components/EditConsent/EditConsent.js
+++ patient/src/components/EditConsent/EditConsent.js
@@ -20,11 +20,45 @@ const EditConsent = () => {
 
     const updateData = useSelector((state) => state.consent.consentToUpdate);
 
-    const [formData, setFormData] = useState(updateForm);
+    const [formData, setFormData] = useState(updateData);
 
     const handleChange = (e) => {
         setFormData({ ...formData, [e.target.name]: e.target.value });
+        if (e.target.name === 'reqStartDate') {
+            setstartdate(e.target.value);
+        } else if (e.target.name === 'reqEndDate') {
+            setenddate(e.target.value);
+        } else if (e.target.name === 'validity') {
+            setvalidity(e.target.value);
+        }
     };
+
+    const [focus, setFocused] = useState(false);
+    const [hasValue, setHasValue] = useState(false);
+    const onFocus = () => setFocused(true);
+    const onBlur = () => setFocused(false);
+
+    const [focus1, setFocused1] = useState(false);
+    const [hasValue1, setHasValue1] = useState(false);
+    const onFocus1 = () => setFocused1(true);
+    const onBlur1 = () => setFocused1(false);
+
+    const [focus2, setFocused2] = useState(false);
+    const [hasValue2, setHasValue2] = useState(false);
+    const onFocus2 = () => setFocused2(true);
+    const onBlur2 = () => setFocused2(false);
+
+    const [startdate, setstartdate] = useState(
+        moment(`${updateData.startDate}`).format('YYYY-MM-DD')
+    );
+
+    const [enddate, setenddate] = useState(
+        moment(`${updateData.endDate}`).format('YYYY-MM-DD')
+    );
+
+    const [validity, setvalidity] = useState(
+        moment(`${updateData.validity}`).format('YYYY-MM-DD')
+    );
 
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -73,68 +107,49 @@ const EditConsent = () => {
                                             Edit Consent
                                         </h2>
                                     </Grid>
-                                    <div>
-                                        <label
-                                            style={{
-                                                display: 'inline-block',
-                                                width: '25%',
-                                                textAlign: 'right',
-                                                color: 'white',
-                                                fontSize: 20,
-                                            }}
-                                        >
-                                            Start Date:
-                                        </label>
-                                        <TextField
-                                            InputProps={{
-                                                disableUnderline: true,
-                                            }}
-                                            style={{
-                                                background: 'white',
-                                                borderRadius: 50,
-                                                margin: '4px',
-                                                height: '40px',
-                                                marginBottom: '30px',
-                                            }}
-                                            name="startDate"
-                                            defaultValue={moment(
-                                                `${updateData.startDate}`
-                                            ).format('YYYY-MM-DD')}
-                                            type="date"
-                                            fullWidth
-                                            required
-                                            onChange={handleChange}
-                                        />
-                                    </div>
-                                    <label
-                                        style={{
-                                            display: 'inline-block',
-                                            width: '25%',
-                                            color: 'white',
-                                            fontSize: 20,
-                                        }}
-                                    >
-                                        End Date:
-                                    </label>
                                     <TextField
+                                        onFocus={onFocus}
+                                        onBlur={onBlur}
                                         InputProps={{ disableUnderline: true }}
                                         style={{
                                             background: 'white',
-                                            borderRadius: 50,
                                             margin: '4px',
-                                            height: '40px',
                                             marginBottom: '30px',
                                         }}
-                                        name="endDate"
-                                        defaultValue={moment(
-                                            `${updateData.endDate}`
-                                        ).format('YYYY-MM-DD')}
-                                        type="date"
+                                        name="reqStartDate"
+                                        label="   Request Start Date"
+                                        type={
+                                            hasValue || focus ? 'date' : 'text'
+                                        }
                                         fullWidth
-                                        required
                                         onChange={handleChange}
+                                        required
+                                        variant="outlined"
+                                        value={startdate}
                                     />
-                                    <label
+                                    <TextField
+                                        onFocus={onFocus1}
+                                        onBlur={onBlur1}
+                                        InputProps={{ disableUnderline: true }}
+                                        style={{
+                                            background: 'white',
+                                            margin: '4px',
+                                            marginBottom: '30px',
+                                        }}
+                                        name="reqEndDate"
+                                        label="   Request End Date"
+                                        type={
+                                            hasValue1 || focus1
+                                                ? 'date'
+                                                : 'text'
+                                        }
+                                        fullWidth
+                                        onChange={handleChange}
+                                        required
+                                        variant="outlined"
+                                        value={enddate}
+                                    />
+                                    {/* <label
                                         style={{
                                             display: 'inline-block',
                                             width: '10px',
@@ -160,34 +175,28 @@ const EditConsent = () => {
                                         fullWidth
                                         required
                                         onChange={handleChange}
-                                    />
-
-                                    <label
-                                        style={{
-                                            display: 'inline-block',
-                                            width: '10px',
-                                            textAlign: 'right',
-                                            color: 'white',
-                                            fontSize: 20,
-                                        }}
-                                    >
-                                        Validity:
-                                    </label>
+                                    /> */}
                                     <TextField
+                                        onFocus={onFocus2}
+                                        onBlur={onBlur2}
                                         InputProps={{ disableUnderline: true }}
                                         style={{
                                             background: 'white',
-                                            borderRadius: 50,
                                             margin: '4px',
-                                            height: '40px',
                                             marginBottom: '30px',
                                         }}
                                         name="validity"
-                                        defaultValue={updateData.validity}
+                                        label="   validity"
+                                        type={
+                                            hasValue2 || focus2
+                                                ? 'date'
+                                                : 'text'
+                                        }
                                         fullWidth
-                                        required
-                                        type="date"
                                         onChange={handleChange}
+                                        required
+                                        variant="outlined"
+                                        value={validity}
                                     />
                                 </Paper>
                             </Grid>
